3c79ffa77c5f1b5c28359aa12b8b4e7c71c63ecf
exatombe <jeremy27.clara22@gmail.com>
2025-04-06 01:22:49 +0200

    Update of Devenv state

--- devenv.nix
+++ devenv.nix
@@ -8,13 +8,12 @@
 
 
   languages.ruby.enable = true;
+
   enterShell = ''
     git --version
     bundle install
     npm ci
   '';
-
-
 
   tasks = {
     "c:watch".exec = "bundle exec jekyll serve --livereload";
